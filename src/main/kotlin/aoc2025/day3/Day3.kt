12d161104bf02cbbbaed6ef2476352c87d0877f5
smahout <me@smahout.nl>
2025-12-03 23:54:38 +0100

    Day 3 a bit faster

--- src/main/kotlin/aoc2025/day3/Day3.kt
+++ src/main/kotlin/aoc2025/day3/Day3.kt
@@ -1,11 +1,12 @@
 package aoc2025.day3
 
 import kotlin.math.pow
-import kotlin.system.measureTimeMillis
+import kotlin.system.measureNanoTime
 
 fun main() {
+    val part = 2
     var input: List<IntArray>
-    val inputTime = measureTimeMillis {
+    val inputTime = measureNanoTime {
         input = {}::class.java
             .getResource("/day3/input.txt")
             ?.readText()
@@ -15,29 +16,49 @@ fun main() {
             }.toIntArray() }
             ?: error("Input not found or invalid")
     }
-    val part1Time = measureTimeMillis {
-        println("Part 1: ${input.sumOf { highestJoltage(it, 2) }}")
+
+    var result = 0L
+    val partTime = measureNanoTime {
+        val joltage = Joltage()
+        result = input.sumOf { joltage.highestJoltage(it, if (part == 2) 12 else 2) }
     }
-    val part2Time = measureTimeMillis {
-        println("Part 2: ${input.sumOf { highestJoltage(it, 12) }}")
-    }
-    println("Read input in $inputTime ms.")
-    println("Part 1 in $part1Time ms.")
-    println("Part 2 in $part2Time ms.")
+
+    println("Read input in ${inputTime / 1_000_000.0} ms.")
+    println("Part $part: found $result in ${partTime / 1_000_000.0} ms.")
 }
 
-fun highestJoltage(batteryBank: IntArray, amount: Int) : Long {
-    val batteries = LongArray(amount)
-    var index = 0
-    for (i in 0 until amount) {
-        val end = batteryBank.size - (amount - 1 - i)
-        val window = batteryBank.slice(index until end)
+class Joltage {
+    val powers = LongArray(20)
 
-        val highest = window.maxOfOrNull { it } ?: 0
-        index += window.indexOf(highest)
-
-        batteries[i] = highest * 10.0.pow(amount - 1 - i).toLong()
-        index++ // move past previously chosen index, to move the window along
+    init {
+        powers[0] = 1
+        for (i in 1 until 20) {
+            powers[i] = powers[i - 1] * 10
+        }
     }
-    return batteries.sumOf { it }
+
+    fun highestJoltage(batteryBank: IntArray, amount: Int): Long {
+        val batteries = LongArray(amount)
+        var index = 0
+        for (i in 0 until amount) {
+            val end = batteryBank.size - (amount - 1 - i)
+            val window = batteryBank.slice(index until end)
+
+            var highest = 0
+            var highestIndex = 0
+            for (i in 0 until window.size) {
+                if (window[i] > highest) {
+                    highest = window[i]
+                    highestIndex = i
+                    if (highest == 9)
+                        break;
+                }
+            }
+            index += highestIndex
+
+            batteries[i] = highest * powers[amount - 1 - i]
+            index++ // move past previously chosen index, to move the window along
+        }
+        return batteries.sumOf { it }
+    }
 }
